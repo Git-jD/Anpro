20ff3d0dce9da5ea418e12119fbf3e7bd47b3c62
Jyoti <kaju47901s@gmail.com>
2021-01-13 22:29:39 +0530

    Scrollable elements added to category

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -20,25 +20,27 @@ class _HomeState extends State<Home> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CustomText(
-                      text: "What are you looking for?",
-                      size: 18,
-                    )),
+                  padding: const EdgeInsets.all(8.0),
+                  child: CustomText(
+                    text: "What are you looking for?",
+                    size: 18,
+                  ),
+                ),
                 Stack(
                   children: <Widget>[
                     IconButton(
                         icon: Icon(Icons.notifications_none), onPressed: () {}),
                     Positioned(
-                        top: 12,
-                        right: 12,
-                        child: Container(
-                          height: 10,
-                          width: 10,
-                          decoration: BoxDecoration(
-                              color: red,
-                              borderRadius: BorderRadius.circular(20)),
-                        ))
+                      top: 12,
+                      right: 12,
+                      child: Container(
+                        height: 10,
+                        width: 10,
+                        decoration: BoxDecoration(
+                            color: red,
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                    ),
                   ],
                 )
               ],
@@ -54,20 +56,63 @@ class _HomeState extends State<Home> {
                     blurRadius: 4)
               ]),
               child: ListTile(
-                  leading: Icon(
-                    Icons.search,
-                    color: red,
-                  ),
-                  title: TextField(
-                    decoration: InputDecoration(
-                        hintText: "find food and restaurent",
-                        border: InputBorder.none),
-                  ),
-                  trailing: Icon(
-                    Icons.filter_list,
-                    color: red,
-                  )),
-            )
+                leading: Icon(
+                  Icons.search,
+                  color: red,
+                ),
+                title: TextField(
+                  decoration: InputDecoration(
+                      hintText: "find food and restaurent",
+                      border: InputBorder.none),
+                ),
+                trailing: Icon(
+                  Icons.filter_list,
+                  color: red,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Container(
+              height: 120,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 15,
+                itemBuilder: (_, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          decoration: BoxDecoration(color: white, boxShadow: [
+                            BoxShadow(
+                                color: Colors.red[50],
+                                offset: Offset(4, 6),
+                                blurRadius: 12)
+                          ]),
+                          child: Padding(
+                            padding: EdgeInsets.all(4),
+                            child: Image.asset(
+                              "images/Food1.jpg",
+                              width: 60,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CustomText(
+                          text: "Salad",
+                          size: 14,
+                          colors: black,
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
